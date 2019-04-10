4a65e15256cc04403b089fec1034c5f3524afefe
Michael Odumosu <michaelodumosu57@gmail.com>
2019-04-10 11:59:56 -0400

    update reqBody, added a finish function for addtiton steps for the end of the stream

--- requestBody.js
+++ requestBody.js
@@ -2,13 +2,33 @@
         /*
             .stream readeable stream for data
             .fn function once done with the steram the req body will be in dev_obj.stream.body
+            .keep whether to keep the created item
+                false deletes created item
+            .finish, since this is a stream last steps must go here
         */
         var reqBody_dev_obj = ultraObject.args.add(   {value:ultraObject.iterify(   {iterify:dev_obj}   )   }   )
-        dev_obj.stream.on('data',(chunk)=> dev_obj.stream.body += chunk)
+        dev_obj.stream.on('data',(chunk)=> {
+            if(   dev_obj.stream.body === undefined  ){
+                dev_obj.stream.body = '';
+            } 
+            dev_obj.stream.body += chunk
+        })
         dev_obj.stream.on('end',function(){
-            rBMisc_0_i =  ultraObject.scope.add(   {value:ultraObject.misc.add(   {value:ultraObject.iterableObject()}   )}   )
-            ultraObject.misc.abelast.add(   {value:ultraObject.scope[rBMisc_0_i]}   )            
+            // rBMisc_0_i =  ultraObject.scope.add(   {value:ultraObject.misc.add(   {value:ultraObject.iterableObject()}   )}   )
+            // ultraObject.misc.abelast.add(   {value:ultraObject.scope[rBMisc_0_i]}   )            
             dev_obj.fn(dev_obj)
-            ultraObject.scope.minus(   {index:rBMisc_0_i}   )
+            if(   dev_obj.keep === 'false'){
+                ultraObject.misc.minus({
+                    index:ultraObject.misc.abelast[
+                        ultraObject.misc.abelast.length-1
+                    ]
+                })
+                ultraObject.misc.abelast.minus({
+                    index:ultraObject.misc.abelast.length-1
+                })                
+            }
+            dev_obj.finish(dev_obj)
+            //why use dev_obj when you know where it is LOL
+            // ultraObject.scope.minus(   {index:rBMisc_0_i}   )
         })
     }// make body data collection easy
--- ultraObject.js
+++ ultraObject.js
@@ -57,7 +57,12 @@
     // O stands for Object for this functionality like thisfn has thisfnO
     // itO stands for iterableObject
     // MB_0_i stands for memory bank used when its hard to contain needed data inside a nested function. its also used as the brain of the API to access every related to its project purpose
-    // q[int] repesentes the access spot for a cloned purpose object in an itO, used to figure out how to access theses
+    /* [function abbrevation][template] for a naming conveation of items in your API
+        for nested function
+        [function abbrevation]_[[function abbrevation]...][template]  
+        when adding the args to ultraObject.args 
+        [function full]_[[function full]...]_dev_obj   
+    */
     // for the remiidify API
         // not sure if calls should be functions or methods which is easier for ultraObject to survivce
     /*
@@ -2690,14 +2695,34 @@
         /*
             .stream readeable stream for data
             .fn function once done with the steram the req body will be in dev_obj.stream.body
+            .keep whether to keep the created item
+                false deletes created item
+            .finish, since this is a stream last steps must go here
         */
         var reqBody_dev_obj = ultraObject.args.add(   {value:ultraObject.iterify(   {iterify:dev_obj}   )   }   )
-        dev_obj.stream.on('data',(chunk)=> dev_obj.stream.body += chunk)
+        dev_obj.stream.on('data',(chunk)=> {
+            if(   dev_obj.stream.body === undefined  ){
+                dev_obj.stream.body = '';
+            } 
+            dev_obj.stream.body += chunk
+        })
         dev_obj.stream.on('end',function(){
-            rBMisc_0_i =  ultraObject.scope.add(   {value:ultraObject.misc.add(   {value:ultraObject.iterableObject()}   )}   )
-            ultraObject.misc.abelast.add(   {value:ultraObject.scope[rBMisc_0_i]}   )            
+            // rBMisc_0_i =  ultraObject.scope.add(   {value:ultraObject.misc.add(   {value:ultraObject.iterableObject()}   )}   )
+            // ultraObject.misc.abelast.add(   {value:ultraObject.scope[rBMisc_0_i]}   )            
             dev_obj.fn(dev_obj)
-            ultraObject.scope.minus(   {index:rBMisc_0_i}   )
+            if(   dev_obj.keep === 'false'){
+                ultraObject.misc.minus({
+                    index:ultraObject.misc.abelast[
+                        ultraObject.misc.abelast.length-1
+                    ]
+                })
+                ultraObject.misc.abelast.minus({
+                    index:ultraObject.misc.abelast.length-1
+                })                
+            }
+            dev_obj.finish(dev_obj)
+            //why use dev_obj when you know where it is LOL
+            // ultraObject.scope.minus(   {index:rBMisc_0_i}   )
         })
     }// make body data collection easy
         
